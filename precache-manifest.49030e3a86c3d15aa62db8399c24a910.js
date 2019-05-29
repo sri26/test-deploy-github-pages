c344d8854a65b58841e056814d5a2102a5063980
sri26 <thefrequent99@gmail.com>
2019-05-29 12:06:09 -0400

    Updates

---
+++ precache-manifest.49030e3a86c3d15aa62db8399c24a910.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "726ee118204c4b20d62a892515303c5f",
+    "url": "/test-deploy-github-pages/index.html"
+  },
+  {
+    "revision": "736f1391d6e1f3f05ce7",
+    "url": "/test-deploy-github-pages/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/test-deploy-github-pages/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "736f1391d6e1f3f05ce7",
+    "url": "/test-deploy-github-pages/static/js/main.360ec479.chunk.js"
+  },
+  {
+    "revision": "8c5aa98ed4a03117e279",
+    "url": "/test-deploy-github-pages/static/js/runtime~main.e69d48cd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-deploy-github-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
